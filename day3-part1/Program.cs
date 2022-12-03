76efa32c4922eb6c795c2bf2afecfbf415d2ec41
Kim <kimheurter21@gmail.com>
2022-12-03 09:47:16 +0100

    Day 3

---
+++ day3-part1/Program.cs
@@ -0,0 +1,22 @@
+﻿var lowerCase = Enumerable.Range('a', 26).ToList();
+var upperCase = Enumerable.Range('A', 26).ToList();
+lowerCase.AddRange(upperCase);
+var scores = lowerCase.Select((x, i) => ((char)x, i + 1)).ToDictionary(x => x.Item1, x => x.Item2);
+
+var input = File.ReadAllLines("input.txt");
+int cumulativeScore = 0;
+foreach(var rucksack in input)
+{
+    var compartment1 = rucksack[..(rucksack.Length / 2)];
+    var compartment2 = rucksack[(rucksack.Length / 2)..];
+    foreach(var type in compartment1)
+    {
+        if (compartment2.Contains(type))
+        {
+            cumulativeScore += scores[type];
+            break;
+        }
+    }
+}
+
+Console.WriteLine($"The total score is {cumulativeScore}");
---
+++ day3-part2/Program.cs
@@ -0,0 +1,21 @@
+﻿var lowerCase = Enumerable.Range('a', 26).ToList();
+var upperCase = Enumerable.Range('A', 26).ToList();
+lowerCase.AddRange(upperCase);
+var scores = lowerCase.Select((x, i) => ((char)x, i + 1)).ToDictionary(x => x.Item1, x => x.Item2);
+
+var input = File.ReadAllLines("input.txt");
+int cumulativeScore = 0;
+
+foreach (var group in input.Chunk(3))
+{
+    foreach (var type in group[0])
+    {
+        if (group[1].Contains(type) && group[2].Contains(type))
+        {
+            cumulativeScore += scores[type];
+            break;
+        }
+    }
+}
+
+Console.WriteLine($"The total score is {cumulativeScore}");
