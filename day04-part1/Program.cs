fe968d2a7e2e96c8cfe59dc353fee742496538f5
Kim <kimheurter21@gmail.com>
2022-12-04 10:20:00 +0100

    Day 4

---
+++ day04-part1/Program.cs
@@ -0,0 +1,8 @@
+﻿using System.Diagnostics;
+
+var input = File.ReadAllLines("input.txt");
+var count = input.Select(x => x.Split(',').Select(y => y.Split('-').Select(int.Parse)))
+    .Count(x => 
+    (x.First().First() >= x.Last().First() && x.First().Last() <= x.Last().Last()) || 
+    (x.First().First() <= x.Last().First() && x.First().Last() >= x.Last().Last()));
+Debug.WriteLine($"The amount of fully contained pairs = {count}");
---
+++ day04-part2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Diagnostics;
+
+var input = File.ReadAllLines("input.txt");
+var count = input.Select(x => x.Split(',').Select(y => y.Split('-').Select(int.Parse)))
+    .Count(x => CheckOverlap(x.First().First(), x.First().Last(), x.Last().First(), x.Last().Last()));
+Debug.WriteLine($"The amount of fully contained pairs = {count}");
+
+bool CheckOverlap(int leftFirst, int leftLast, int rightFirst, int rightLast)
+{
+    return  (leftFirst >= rightFirst && leftFirst <= rightLast) ||
+            (leftLast >= rightFirst && leftLast <= rightLast) ||
+            (rightFirst >= leftFirst && rightFirst <= leftLast) ||
+            (rightLast >= leftFirst && rightLast <= leftLast);
+}
