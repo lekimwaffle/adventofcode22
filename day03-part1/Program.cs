9357c9be2880ee4a88d7575a5cdb7cfac4e7db11
Kim <kimheurter21@gmail.com>
2022-12-04 13:38:36 +0100

    Simplified day 3

--- day03-part1/Program.cs
+++ day03-part1/Program.cs
@@ -2,21 +2,10 @@
 var upperCase = Enumerable.Range('A', 26).ToList();
 lowerCase.AddRange(upperCase);
 var scores = lowerCase.Select((x, i) => ((char)x, i + 1)).ToDictionary(x => x.Item1, x => x.Item2);
+scores.Add(default, 0);
 
 var input = File.ReadAllLines("input.txt");
-int cumulativeScore = 0;
-foreach(var rucksack in input)
-{
-    var compartment1 = rucksack[..(rucksack.Length / 2)];
-    var compartment2 = rucksack[(rucksack.Length / 2)..];
-    foreach(var type in compartment1)
-    {
-        if (compartment2.Contains(type))
-        {
-            cumulativeScore += scores[type];
-            break;
-        }
-    }
-}
+int cumulativeScore = input.Select(x => (First: x[..(x.Length / 2)], Second: x[(x.Length / 2)..]))
+    .Sum(y => scores[y.First.FirstOrDefault(x => y.Second.Contains(x))]);
 
 Console.WriteLine($"The total score is {cumulativeScore}");
--- day03-part2/Program.cs
+++ day03-part2/Program.cs
@@ -2,20 +2,11 @@
 var upperCase = Enumerable.Range('A', 26).ToList();
 lowerCase.AddRange(upperCase);
 var scores = lowerCase.Select((x, i) => ((char)x, i + 1)).ToDictionary(x => x.Item1, x => x.Item2);
+scores.Add(default, 0);
 
 var input = File.ReadAllLines("input.txt");
-int cumulativeScore = 0;
-
-foreach (var group in input.Chunk(3))
-{
-    foreach (var type in group[0])
-    {
-        if (group[1].Contains(type) && group[2].Contains(type))
-        {
-            cumulativeScore += scores[type];
-            break;
-        }
-    }
-}
+int cumulativeScore = input.Chunk(3)
+    .Select(x => (Source: x[0], Group: x[^2..]))
+    .Sum(x => scores[x.Source.FirstOrDefault(y => x.Group.All(z => z.Contains(y)))]);
 
 Console.WriteLine($"The total score is {cumulativeScore}");
