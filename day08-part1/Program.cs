364330167b265e3d7afffe9eca5eea0c29ce5de8
Kim <kimheurter21@gmail.com>
2022-12-08 22:43:11 +0100

    Day 8

---
+++ day08-part1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics;
+
+var input = File.ReadAllLines("input.txt");
+var map = new int[input.Length, input[0].Length];
+for (int x = 0; x < input.Length; x++)
+    for (int y = 0; y < input[0].Length; y++)
+        map[x, y] = input[y][x] - '0';
+
+int visibleTrees = Enumerable.Range(0, input.Length)
+                    .SelectMany(x => Enumerable.Range(0, input[0].Length).Select(y => (X: x, Y: y)))
+                    .Count(p => p.X == 0 || p.Y == 0 || p.X == input.Length - 1 || p.Y == input[0].Length - 1 ||
+                                LookUpWest(p.X, p.Y) || LookUpNorth(p.X, p.Y) || LookUpEast(p.X, p.Y) || LookUpSouth(p.X, p.Y));
+
+bool LookUpWest(int startX, int startY) => Enumerable.Range(0, startX).All(x => map[x, startY] < map[startX, startY]);
+bool LookUpEast(int startX, int startY) => Enumerable.Range(1, input.Length - 1 - startX).All(x => map[startX + x, startY] < map[startX, startY]);
+bool LookUpNorth(int startX, int startY) => Enumerable.Range(0, startY).All(y => map[startX, y] < map[startX, startY]);
+bool LookUpSouth(int startX, int startY) => Enumerable.Range(1, input[0].Length - 1 - startY).All(y => map[startX, startY + y] < map[startX, startY]);
+
+Debug.WriteLine($"Amount of visible trees is {visibleTrees}");
---
+++ day08-part2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics;
+
+var input = File.ReadAllLines("input.txt");
+var map = new int[input.Length, input[0].Length];
+for (int x = 0; x < input.Length; x++)
+    for (int y = 0; y < input[0].Length; y++)
+        map[x, y] = input[y][x] - '0';
+
+int highestViewingScore = Enumerable.Range(1, input.Length - 2)
+                    .SelectMany(x => Enumerable.Range(1, input[0].Length - 2).Select(y => (X: x, Y: y)))
+                    .Max(p => LookUpWest(p.X, p.Y) * LookUpNorth(p.X, p.Y) * LookUpEast(p.X, p.Y) * LookUpSouth(p.X, p.Y));
+
+int LookUpWest(int startX, int startY) => startX - Enumerable.Range(0, startX).Reverse().FirstOrDefault(x => map[x, startY] >= map[startX, startY]);
+int LookUpEast(int startX, int startY) => Enumerable.Range(startX + 1, input.Length - 2 - startX).FirstOrDefault(x => map[x, startY] >= map[startX, startY], input.Length - 1) - startX;
+int LookUpNorth(int startX, int startY) => startY - Enumerable.Range(0, startY).Reverse().FirstOrDefault(y => map[startX, y] >= map[startX, startY]);
+int LookUpSouth(int startX, int startY) => Enumerable.Range(startY + 1, input.Length - 2 - startY).FirstOrDefault(y => map[startX, y] >= map[startX, startY], input[0].Length - 1) - startY;
+
+Debug.WriteLine($"The highest viewing score is {highestViewingScore}");
