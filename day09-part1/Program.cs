1878c480fca57942360b0fbd9a4e88cb5f55e1a0
Kim <kimheurter21@gmail.com>
2022-12-09 08:09:22 +0100

    Day 9

---
+++ day09-part1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Diagnostics;
+
+var input = File.ReadAllLines("input.txt");
+Coord head = new(0, 0);
+Coord tail = new(0, 0);
+var visited = new HashSet<Coord> { tail };
+foreach (var line in input)
+{
+    Func<Coord, Coord> moveFunction = line[0] switch
+    {
+        'R' => c => new (c.X + 1, c.Y),
+        'L' => c => new (c.X - 1, c.Y),
+        'D' => c => new (c.X, c.Y + 1),
+        'U' => c => new (c.X, c.Y - 1),
+    };
+
+    for(int step = 0; step < int.Parse(line[2..]); step++)
+    {
+        // Move head
+        head = moveFunction(head);
+        tail = head switch
+        {
+            _ when head.X > tail.X + 1 => new Coord(tail.X + 1, head.Y),
+            _ when head.X < tail.X - 1 => new Coord(tail.X - 1, head.Y),
+            _ when head.Y > tail.Y + 1 => new Coord(head.X, tail.Y + 1),
+            _ when head.Y < tail.Y - 1 => new Coord(head.X, tail.Y - 1),
+            _ => tail
+        };
+
+        if (!visited.Contains(tail))
+            visited.Add(tail);
+    }
+}
+
+Debug.WriteLine($"Amount of visited tiles is {visited.Count}");
+
+record Coord(int X, int Y);
---
+++ day09-part2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Diagnostics;
+
+var input = File.ReadAllLines("input.txt");
+Coord[] knots = Enumerable.Range(0, 10).Select(x => new Coord(0,0)).ToArray();
+var visited = new HashSet<Coord> { knots.Last() };
+foreach (var line in input)
+{
+    Func<Coord, Coord> moveFunction = line[0] switch
+    {
+        'R' => c => new(c.X + 1, c.Y),
+        'L' => c => new(c.X - 1, c.Y),
+        'D' => c => new(c.X, c.Y + 1),
+        'U' => c => new(c.X, c.Y - 1),
+    };
+
+    for (int step = 0; step < int.Parse(line[2..]); step++)
+    {
+        knots[0] = moveFunction(knots[0]);
+        for(int knot = 0; knot < knots.Length - 1; knot++)
+            knots[knot + 1] = Move(knots[knot], knots[knot + 1]);
+
+        if (!visited.Contains(knots.Last()))
+            visited.Add(knots.Last());
+    }
+}
+
+static Coord Move(Coord lead, Coord follower) => lead switch
+{
+    _ when lead.X > follower.X + 1 && lead.Y > follower.Y + 1 => new Coord(follower.X + 1, follower.Y + 1),
+    _ when lead.X < follower.X - 1 && lead.Y > follower.Y + 1 => new Coord(follower.X - 1, follower.Y + 1),
+    _ when lead.Y < follower.Y - 1 && lead.X < follower.X - 1 => new Coord(follower.X - 1, follower.Y - 1),
+    _ when lead.Y < follower.Y - 1 && lead.X > follower.X + 1 => new Coord(follower.X + 1, follower.Y - 1),
+    _ when lead.X > follower.X + 1 => new Coord(follower.X + 1, lead.Y),
+    _ when lead.X < follower.X - 1 => new Coord(follower.X - 1, lead.Y),
+    _ when lead.Y > follower.Y + 1 => new Coord(lead.X, follower.Y + 1),
+    _ when lead.Y < follower.Y - 1 => new Coord(lead.X, follower.Y - 1),
+    _ => follower
+};
+
+Debug.WriteLine($"Amount of visited tiles is {visited.Count}");
+
+record Coord(int X, int Y);
